680beef824e140636fdf0a022da87cd949542939
TruongDoan <doantruong120699@gmail.com>
2021-03-06 16:24:06 +0700

    Create table profile

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from vietnam_provinces import Province, District, Ward
+from vietnam_provinces.enums import ProvinceEnum
 
-# Create your models here.
+# ==============================
+# === Custom cities_light model ===
+class VietNamProvince(models.Model):
+    province = ProvinceEnum
+# === custom cities_light model ===
+# ==============================
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+
+class Position(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+
+class Area(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    birthday = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    identify_card = models.CharField(max_length=200, null=True, blank=True)
+    gender = models.BooleanField(default=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True, null=True, blank=True)
+    faculty = models.ForeignKey(Faculty, related_name = 'faculty_profile', on_delete=models.SET_NULL, blank=True, null=True)
+    position = models.ForeignKey(Faculty, related_name = 'position_profile', on_delete=models.SET_NULL, blank=True, null=True)
+    area = models.ForeignKey(Faculty, related_name = 'area_profile', on_delete=models.SET_NULL, blank=True, null=True)
