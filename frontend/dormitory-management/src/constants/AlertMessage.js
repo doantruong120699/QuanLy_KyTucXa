86b8e176ebdb94d83b7deff40c5af8afcaf2f0da
Phan Ben <ben_phan@datahouse.asia>
2021-03-11 22:20:30 +0700

    user-login

---
+++ frontend/dormitory-management/src/constants/AlertMessage.js
@@ -0,0 +1,8 @@
+export const ACCOUNT_EMPTY = "Tài khoản không được để trống!";
+export const ACCOUNT_PASSWORD_UNAVAILABLE =
+  "Tài khoản hoặc mật khẩu không đúng";
+export const EMAIL_INVALID = "Email không hợp lệ!";
+export const EMAIL_UNAVAILABLE = "Email không đúng!";
+export const NONE_MESSAGE = "";
+export const PASSWORD_INVALID = "Mật khẩu từ 8 kí tự bao gồm chữ và số!";
+export const PASSWORD_DIFFERENT = "Mật khẩu không khớp!";
---
+++ frontend/dormitory-management/src/constants/utilities.js
@@ -0,0 +1,22 @@
+export const NAVIGATION_TITLE = [
+  {
+    title: "Dashboard",
+    path: "./dashboard",
+  },
+  {
+    title: "Xem phòng",
+    path: "./room",
+  },
+  {
+    title: "Sinh viên",
+    path: "./",
+  },
+  {
+    title: "Nhân viên",
+    path: "./",
+  },
+  {
+    title: "Dịch vụ",
+    path: "./",
+  },
+];
