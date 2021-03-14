890b6913a95cbf2671b2635d8fa8a28d8e8d25b2
Quang <asdquang111@gmail.com>
2021-03-14 18:46:48 +0700

    quang#changepassword

--- mobile_app/dormitorymanagement/src/components/login/Login.js
+++ mobile_app/dormitorymanagement/src/components/login/Login.js
@@ -37,7 +37,7 @@ class Login extends Component {
             this.showToast('Email or password is empty!');
             return;
         }
-        const data = {"username": "quang1@gmail.com", "password": "123456"};
+        const data = {"username": "asdquang111@gmail.com", "password": "quang1999"};
         await this.props.login(data);
         const token = await getData('token');
         // const role = await getData('role');
--- mobile_app/dormitorymanagement/src/redux/reducer/index.js
+++ mobile_app/dormitorymanagement/src/redux/reducer/index.js
@@ -2,5 +2,6 @@ import { login } from './login';
 import { getallroom } from './getallroom';
 import { searchroom } from './searchroom';
 import { mainmenu } from './mainmenu';
+import { changepassword } from './changepassword';
 
-export { login, getallroom, searchroom, mainmenu };
+export { login, getallroom, searchroom, mainmenu, changepassword };
