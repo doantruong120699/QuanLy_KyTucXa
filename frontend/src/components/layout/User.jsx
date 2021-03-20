2d935761cc895154bbd128e101d04d3abd6f4667
Phan Ben <ben_phan@datahouse.asia>
2021-03-20 23:23:21 +0700

    benphan#UI-Info-user

--- frontend/src/components/layout/User.jsx
+++ frontend/src/components/layout/User.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import * as ROUTER from "../../utilities/constants/router";
 
 const User = () => {
   const [isShown, setIsShown] = useState(false);
 
+  const history = useHistory();
   const logoutWrapper = useRef(null);
 
   const useClickOutside = (ref) => {
@@ -35,12 +38,15 @@ const User = () => {
       </div>
       {isShown && (
         <div className="style-dropdownContainer">
-          <div className="style-dropdownItem">
+          <div
+            className="style-dropdownItem"
+            onClick={() => history.push(ROUTER.ROUTE_MY_PROFILE)}
+          >
             <i className="fi-sr-user" />
             <span>Trang cá nhân</span>
           </div>
           <div className="style-dropdownItem">
-            <i class="fi-sr-sign-out" />
+            <i className="fi-sr-sign-out" />
             <span className="">Đăng xuất</span>
           </div>
         </div>
