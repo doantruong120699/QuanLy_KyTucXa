c5a4f3a0090dfe7ac16489a953a1cfa502d3608f
Phan Ben <ben_phan@datahouse.asia>
2021-03-26 01:07:31 +0700

    benphan#fix-edit-profile

--- frontend/src/components/profile/EditProfile.jsx
+++ frontend/src/components/profile/EditProfile.jsx
@@ -1,10 +1,12 @@
 import Popup from "reactjs-popup";
+import Button from "../common/Button";
 import FormError from "../common/FormError";
 import InputField from "../common/InputField";
 
-const EditProfile = () => {
+const EditProfile = (props) => {
+  const { open, onClose, saveInfo } = props;
   return (
-    <Popup open={true} closeOnDocumentClick onClose={""}>
+    <Popup open={open} closeOnDocumentClick onClose={onClose}>
       <div className="col modal style-lg-box bg-color-white text-align-ct">
         <h2 className="pt-12">Nhập thông tin cá nhân mới</h2>
         <div className="col col-full pd-24">
@@ -12,7 +14,7 @@ const EditProfile = () => {
             <FormError isHidden={""} errorMessage={""} />
             <InputField
               name="curPass"
-              isValid={""}
+              isValid={true}
               type="text"
               placeholder="Tên hiển thị"
               onChange={""}
@@ -23,7 +25,7 @@ const EditProfile = () => {
             <FormError isHidden={""} errorMessage={""} />
             <InputField
               name="curPass"
-              isValid={""}
+              isValid={true}
               type="text"
               placeholder="Địa chỉ"
               onChange={""}
@@ -31,10 +33,9 @@ const EditProfile = () => {
             />
           </div>
           <div className="col col-full mt-24">
-            <FormError isHidden={""} errorMessage={""} />
             <InputField
               name="curPass"
-              isValid={""}
+              isValid={true}
               type="date"
               placeholder="Ngày sinh"
               onChange={""}
@@ -45,7 +46,7 @@ const EditProfile = () => {
             <FormError isHidden={""} errorMessage={""} />
             <InputField
               name="curPass"
-              isValid={""}
+              isValid={true}
               type="tel"
               placeholder="Số điện thoại"
               onChange={""}
@@ -56,12 +57,52 @@ const EditProfile = () => {
             <FormError isHidden={""} errorMessage={""} />
             <InputField
               name="curPass"
-              isValid={""}
+              isValid={true}
               type="tel"
               placeholder="CMND"
               onChange={""}
               autocomplete="off"
             />
+          </div>
+          <div className="col col-full mt-24">
+            <div className="col col-third pr-4">
+              <InputField
+                name="curPass"
+                isValid={true}
+                type="tel"
+                placeholder="Lớp"
+                onChange={""}
+                autocomplete="off"
+              />
+            </div>
+            <div className="col col-two-third pl-4">
+              <InputField
+                name="curPass"
+                isValid={true}
+                type="tel"
+                placeholder="Ngành"
+                onChange={""}
+                autocomplete="off"
+              />
+            </div>
+          </div>
+          <div className="col col-full mt-24">
+            <div className="col col-half">
+              <Button
+                type="normal-red"
+                content="HỦY"
+                isDisable={false}
+                onClick={onClose}
+              />
+            </div>
+            <div className="col col-half">
+              <Button
+                type="normal-ubg"
+                content="LƯU"
+                isDisable={false}
+                onClick={saveInfo}
+              />
+            </div>
           </div>
         </div>
       </div>
