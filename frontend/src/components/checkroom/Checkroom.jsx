920c55e29e7563de0a5207f7eb264de12ec6fdcf
Phan Ben <ben_phan@datahouse.asia>
2021-03-25 16:17:18 +0700

    benphan#UI-Get-Rooms

--- frontend/src/components/checkroom/Checkroom.jsx
+++ frontend/src/components/checkroom/Checkroom.jsx
@@ -1,4 +1,5 @@
 import { allRooms } from "../dashboard/Data";
+import Room from "../checkroom/Room";
 const Checkroom = () => {
   const rooms = allRooms;
   const dataRooms = rooms.map((data, index) => {
@@ -7,19 +8,13 @@ const Checkroom = () => {
         <p>{data.area}</p>
         {data.roomsOfArea.map((room, value) => {
           return (
-            <div className="col col-5">
-              <div key={value} className="col style-roomOfArea style-sm-box">
-                <p>{room.name}</p>
-                {room.students.map((student, index) => {
-                  return (
-                    <div className="col col-4">
-                      <div className="style-student">
-                        <i className="fi-sr-user"></i>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
+            <div className="col col-5 pd-8">
+              <Room
+                key={value}
+                name={room.name}
+                maximum={room.number}
+                students={room.students}
+              />
             </div>
           );
         })}
