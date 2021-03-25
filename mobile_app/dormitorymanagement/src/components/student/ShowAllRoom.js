5114dc9af37dec01fd3823ec29648d6869f4203f
truongquanghung <truongquanghunghn@gmail.com>
2021-03-25 23:08:41 +0700

    Re-Design show room

--- mobile_app/dormitorymanagement/src/components/student/ShowAllRoom.js
+++ mobile_app/dormitorymanagement/src/components/student/ShowAllRoom.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, FlatList, TouchableOpacity, Text, SectionList, ImageBackground} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, TouchableOpacity, Text, SectionList, ImageBackground, ToastAndroid, } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { connect } from 'react-redux';
 import { getallroom } from '../../redux/actions/getallroom';
 import { searchroom } from '../../redux/actions/searchroom';
 import { AppBar } from '../index';
 import Room from './Room';
+import { useEffect } from 'react';
 // const Item = ({ item, onPress, style }) => (
 //     <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
 //         <Text style={styles.title}>{item.title}</Text>
@@ -18,110 +20,141 @@ import Room from './Room';
 //         />
 //     );
 // };
-class ShowAllRoom extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.data,
-    }
+const ShowAllRoom = (props) => {
+  const { data, navigation } = props;
+  const handleClickItem = () => {
+    console.log("Clicked");
   }
-    renderItem = ({ item }) => {
-      let result;
-      return (
-        <Room myRoom={item}/>
-      )
-    }
-    render() {
-      // console.log(this.state.data);
-        return (
-            <View style={styles.container}>
-                <ImageBackground source={require('../../assets/background.jpg')} style={styles.imageBackground}>
-                    <AppBar style={styles.appBar} navigation={this.props.navigation}/>
-                    <View style={styles.container_child}>
-                        <SectionList
-                            style={styles.sectionList}
-                            sections={this.state.data}
-                            keyExtrator={(item, index) => item+index}
-                            renderItem={this.renderItem}
-                            renderSectionHeader={({ section: { title }}) => (
-                              <Text style={styles.header}>{title}</Text>
-                            )}
-                        />
-                    </View>
-                </ImageBackground>
-            </View>
-        )
-    }
-}
+  const renderItem = ({ item }) => {
+    let result;
+    return (
+      <Room myRoom={item} onClick={handleClickItem} />
+    )
+  }
+  const [selectedValue, setSelectedValue] = useState("All");
+  const [loading, setLoading] = useState(false);
+  const loadCurrentData = () => {
+    const result = data.filter(t => t.title === selectedValue)
+    setSectionData(...[result]);
 
+  }
+  const [sectionData, setSectionData] = useState(data);
+  useEffect(() => {
+    loadCurrentData();
+  }, [])
+  const changeSelectedValueItem = (item) => {
+    setSelectedValue(item);
+  }
+  const handlePickerChange = async (item) => {
+    changeSelectedValueItem(item);
+
+  }
+  return (
+    <View style={styles.container}>
+
+      <ImageBackground source={require('../../assets/background.jpg')} style={styles.imageBackground}>
+        <AppBar style={styles.appBar} navigation={navigation} onChange={(t) => {
+        }
+        } />
+        <View style={styles.container}>
+          <Picker
+            selectedValue={selectedValue}
+            style={{ height: 100, width: 150 }}
+            onValueChange={(itemValue, itemIndex) => {
+              handlePickerChange(itemValue);
+            }}
+          >
+            <Picker.Item label="Khu A" value="Khu A" />
+            <Picker.Item label="Khu B" value="Khu B" />
+            <Picker.Item label="Khu C" value="Khu C" />
+            <Picker.Item label="Tat ca" value="All" />
+          </Picker>
+        </View>
+        <View style={styles.container_child}>
+          <SectionList
+            style={styles.sectionList}
+            sections={selectedValue === "All" ? data : data.filter(t => t.title === selectedValue)}
+            keyExtrator={(item, index) => item + index}
+            renderItem={
+              renderItem
+            }
+            renderSectionHeader={({ section: { title } }) => (
+              <Text style={styles.header}>{title}</Text>
+            )}
+          />
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
 const mapDispatchToProps = {
   getallroom,
 };
 function mapStateToProps(state) {
   return {
-      textSearch: state.searchroom.textSearch,
-      data: state.getallroom.data,
-      filter_status: state.getallroom.filter_status,
+    textSearch: state.searchroom.textSearch,
+    data: state.getallroom.data,
+    filter_status: state.getallroom.filter_status,
   };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShowAllRoom);
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-    },
-    imageBackground: {
-      flex: 1,
-      width: '100%',
-      justifyContent: 'center',
-      resizeMode: "cover",
-      alignItems: 'center',
-    },
-    appBar: {
-      flex: 1,
-    },
-    container_child: {
-      flex: 9, 
-      width: '100%',
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    sectionList: {
-      elevation: 7,
-      borderRadius: 20,
-      backgroundColor: 'white',
-      width: '90%',
-      marginBottom: '2%',
-    },
-    item: {
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-    header: {
-      textAlign: 'center',
-      fontSize: 20,
-    },
-    item: {
-      marginLeft: 20,
-      marginRight: 20,
-      backgroundColor: 'yellow',
-      borderRadius: 15,
-      marginBottom: 5,
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    iconitem: {
-      padding: 5,
-      fontSize: 15,
-    },
-    title: {
-      padding: 5,
-      fontSize: 15,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  imageBackground: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    resizeMode: "cover",
+    alignItems: 'center',
+  },
+  appBar: {
+    flex: 1,
+  },
+  container_child: {
+    flex: 9,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionList: {
+    elevation: 7,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    width: '90%',
+    marginBottom: '2%',
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  item: {
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: 'yellow',
+    borderRadius: 15,
+    marginBottom: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconitem: {
+    padding: 5,
+    fontSize: 15,
+  },
+  title: {
+    padding: 5,
+    fontSize: 15,
+  },
 });
