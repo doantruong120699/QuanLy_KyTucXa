e6a51ac616c9edf2dbc6e62d4233d29b3e40facb
TruongDoan <doantruong120699@gmail.com>
2021-04-15 17:49:16 +0700

    API lich lam nhan vien

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,8 @@ import uuid
 from model_utils import Choices
 from django.utils.translation import ugettext_lazy as _
 from django.utils.crypto import get_random_string
+import shortuuid
+import datetime
 
 def unique_slugify(instance, slug):
     model = instance.__class__
@@ -155,4 +157,69 @@ class Contract(models.Model):
     def __str__(self):
         return self.profile.user.username + ' - ' + self.room.name
 
+# =========================================
+
+class Notification(models.Model):
+    public_id = models.CharField(max_length=100, null=True, blank=True, default=shortuuid.uuid(), unique=True)
+    created_by = models.ForeignKey(User, related_name = 'notification_created_by', on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True, null=True, blank=True)
+    updated_by = models.ForeignKey(User, related_name = 'notification_updated_by', on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+#  Ca truc
+class Shift(models.Model):
+    WEEKDAYS = Choices(
+        ('Mon', _('Monday')),
+        ('Tue', _('Tuesday')),
+        ('Wed', _('Wednesday')),
+        ('Thu', _('Thursday')),
+        ('Fri', _('Friday')),
+        ('Sat', _('Saturday')),
+        ('Sun', _('Sunday')),
+    )
+    name = models.CharField(max_length=100, null=True, blank=True)
+    weekdays = models.CharField(choices=WEEKDAYS, max_length=10, null=True, blank=True)
+    start_at = models.TimeField(null=True, blank=True)
+    end_at = models.TimeField(null=True, blank=True)
+    slug = models.CharField(max_length=255, null=True, unique=True)
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name + ' - ' +  self.weekdays
+
+
+class DailySchedule(models.Model):
+    public_id = models.CharField(max_length=100, null=True, blank=True, default=shortuuid.uuid(), unique=True)
+    created_by = models.ForeignKey(User, related_name = 'schedule_created_by', on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True, null=True, blank=True)
+    updated_by = models.ForeignKey(User, related_name = 'schedule_updated_by', on_delete=models.CASCADE, blank=True, null=True)
+    # 
+    YEAR_CHOICES = []
+    for r in range(2010, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r,r))
+
+    YEAR_CHOICES = []
+    for r in range(2010, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r,r))
+
+    WEEk_CHOICES = []
+    d = (datetime.datetime.now().isocalendar()[1])
+    for r in range(d, 53):
+        WEEk_CHOICES.append((r,r))
+
+    week = models.IntegerField(_('week'), choices=WEEk_CHOICES, default=datetime.datetime.now().isocalendar()[1] + 1)
+    year = models.IntegerField(_('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    shift = models.ForeignKey(Shift, related_name = 'schedule_shift', on_delete=models.CASCADE, blank=True, null=True)
+    staff = models.ForeignKey(User, related_name = 'daily_schedule_staff', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title + ' (week:  ' + str(self.week) + ' - ' + str(self.shift) + ' - ' + str(self.staff.user.username) + ')'
 
--- backend/api/nhanvien/urls.py
+++ backend/api/nhanvien/urls.py
@@ -18,7 +18,31 @@ nhanvien_detail = NhanVienViewSet.as_view({
     'get': 'retrieve' # get detail
 })
 
+shift_list = ShiftViewSet.as_view({
+    'get': 'list', # Get lists
+})
+
+shift_detail = ShiftViewSet.as_view({
+    'get': 'retrieve',
+})
+
+
+schedule_list = DailyScheduleViewSet.as_view({
+    'get': 'list', # Get lists
+})
+
+schedule_detail = DailyScheduleViewSet.as_view({
+    'get': 'retrieve',
+})
+
+
 urlpatterns = [
     path('staffs/get-all-staff/', nhanvien_list, name = 'nhanvien_list'),
     path('staffs/<uuid:public_id>/', nhanvien_detail, name = 'nhanvien_detail'),
+    # ========== shift
+    path('shifts/', shift_list, name = 'shift_list'),
+    path('shifts/<slug:slug>/', shift_detail, name = 'shift_detail'),
+    # ========== shift
+    path('daily-schedules/<int:week>/', schedule_list, name = 'schedule_list'),
+    path('daily-schedules/<str:public_id>/', schedule_detail, name = 'schedule_detail'),
 ]
