ab3fe26923defd2aa4373f2049c8f7d0623283b7
TruongDoan <doantruong120699@gmail.com>
2021-04-22 14:46:20 +0700

    fix api daily schedule

--- backend/api/nhanvien/serializers.py
+++ backend/api/nhanvien/serializers.py
@@ -76,7 +76,7 @@ class DailyScheduleListSerializer(serializers.ModelSerializer):
     shift = ShiftSerializer()
     class Meta:
         model = DailySchedule
-        fields = [ "public_id", "week", "title", "content", "shift", "staff"]
+        fields = [ "public_id", "week", "year", "title", "content", "shift", "staff"]
 
 class DailyScheduleDetailSerializer(serializers.ModelSerializer):
     shift = ShiftSerializer()
