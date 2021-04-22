f793744b9fe4e1e9b93c521d9632b10145b93ffc
Phan Ben <benphan1999@gmai.com>
2021-04-22 22:17:13 +0700

    benphan#Schedule-UI

--- frontend/src/redux/actions/checkroom.js
+++ frontend/src/redux/actions/checkroom.js
@@ -53,3 +53,56 @@ export async function getRoomDetails(slug, resolve = () => {}) {
     });
   }
 }
+export async function getPaymentMethods(resolve = () => {}) {
+  store.dispatch({
+    type: types.GET_PAYMENT_METHODS_API,
+  });
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/payment-method/", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    resolve(data);
+    store.dispatch({
+      payload: data,
+      type: types.GET_PAYMENT_METHODS_API_SUCCEED,
+    });
+  } catch (error) {
+    store.dispatch({
+      payload: error,
+      type: types.GET_PAYMENT_METHODS_API_FAIL,
+    });
+  }
+}
+export async function registerRoom(registrationData, resolve = () => {}) {
+  store.dispatch({
+    type: types.POST_REGISTRATION_API,
+  });
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/contracts/", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(registrationData),
+    });
+    const data = await response.json();
+    resolve(data);
+    store.dispatch({
+      payload: data,
+      type: types.POST_REGISTRATION_API_SUCCEED,
+    });
+  } catch (error) {
+    store.dispatch({
+      payload: error,
+      type: types.POST_REGISTRATION_API_FAIL,
+    });
+  }
+}
--- frontend/src/redux/reducers/studentPage.js
+++ frontend/src/redux/reducers/studentPage.js
@@ -2,7 +2,7 @@ import * as types from "../constants";
 
 const initialState = {
   listStudent: {},
-  currentStudent:{},
+  currentStudent: {},
   error: {},
   loading: false,
 };
@@ -10,6 +10,7 @@ const initialState = {
 export default function employeePage(state = initialState, actions) {
   switch (actions.type) {
     case types.GET_STUDENTS_API:
+    case types.GET_DETAILED_STUDENT_API:
       return {
         ...state,
         loading: true,
@@ -21,30 +22,19 @@ export default function employeePage(state = initialState, actions) {
         listStudent: actions.payload,
         loading: false,
       };
+    case types.GET_DETAILED_STUDENT_API_SUCCEED:
+      return {
+        ...state,
+        currentStudent: actions.payload,
+        loading: false,
+      };
+    case types.GET_DETAILED_STUDENT_API_FAIL:
     case types.GET_STUDENTS_API_FAIL:
       return {
         ...state,
         error: actions.payload,
         loading: false,
       };
-      case types.GET_DETAILED_STUDENT_API:
-        return {
-          ...state,
-          loading: true,
-          error: {},
-        };
-      case types.GET_DETAILED_STUDENT_API_SUCCEED:
-        return {
-          ...state,
-          currentStudent  : actions.payload,
-          loading: false,
-        };
-      case types.GET_DETAILED_STUDENT_API_FAIL:
-        return {
-          ...state,
-          error: actions.payload,
-          loading: false,
-        };
     default:
       return state;
   }
