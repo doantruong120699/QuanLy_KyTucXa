29df82409881c7827b5f31a828b743c0a641171c
TruongDoan <doantruong120699@gmail.com>
2021-05-16 22:35:32 +0700

    API WaterElectrical

--- backend/api/quanlytaichinh/serializers.py
+++ backend/api/quanlytaichinh/serializers.py
@@ -9,6 +9,8 @@ from api.serializers import *
 from api.room.serializers import *
 from datetime import date
 from datetime import datetime
+from django.db.models import Q
+required_message = 'This Field is required!'
 
 class StringListField(serializers.ListField): # get from http://www.django-rest-framework.org/api-guide/fields/#listfield
     child = serializers.CharField()
@@ -97,10 +99,10 @@ class WaterElectricalSerializer(serializers.ModelSerializer):
                 new_index_eclectrical=validated_data['new_index_eclectrical'],
                 new_index_water=validated_data['new_index_water'],
                 month = validated_data['month'],
-                water_electrical_unit_price=validated_data['water_electrical_unit_price'],
+                water_electrical_unit_price=water_electrical_unit_price,
                 created_by=current_user
             )
-            d = datetime.datetime.now()
+            d = datetime.now()
             if 'year' in validated_data:
                 year = validated_data['year']
             else:
@@ -112,27 +114,127 @@ class WaterElectricalSerializer(serializers.ModelSerializer):
                 water_electrical_pre_month = WaterElectrical.objects.filter(room=room, year=year-1,month=12)
             else:
                 water_electrical_pre_month = WaterElectrical.objects.filter(room=room, year=year,month=month-1)
-            model.old_index_water = water_electrical_pre_month.new_index_water
-            model.old_index_electrical = water_electrical_pre_month.new_index_eclectrical
-
+            if len(water_electrical_pre_month) == 0:
+                model.old_index_water = 0
+                model.old_index_electrical = 0
+            else:
+                model.old_index_water = water_electrical_pre_month.new_index_water
+                model.old_index_electrical = water_electrical_pre_month.new_index_eclectrical
             model.save()
+
+            bill = Bill.objects.create(
+                public_id=shortuuid.uuid(),
+                water_electrical=model,
+                created_by=current_user,
+            )
+            bill.save()
             return True
-        except:
+        except Exception as e:
+            print(e)
             pass
         return False
 
     def update(self, instance, validated_data):
         try:
             current_user = self._current_user()
-            instance.title = validated_data.get('title', instance.title)
-            instance.content = validated_data.get('content', instance.content)
-            instance.is_display = validated_data.get('is_display', instance.is_display)
-            print(current_user)
+            water_electrical_unit_price = WaterElectricalUnitPrice.objects.get(pk=validated_data['water_electrical_unit_price'])
+            room = Room.objects.get(pk=validated_data['room'])
+
+            instance.room = room
+            instance.new_index_eclectrical = validated_data.get('new_index_eclectrical', instance.new_index_eclectrical)
+            instance.new_index_water = validated_data.get('new_index_water', instance.new_index_water)
+            instance.month = validated_data.get('month', instance.month)
+            if 'year' in validated_data:
+                year = validated_data['year']
+                instance.year = validated_data.get('year', instance.year)
+            instance.water_electrical_unit_price = water_electrical_unit_price
             instance.updated_by = current_user
+
             instance.save()
             return True
         except Exception as e:
             print(e)
             return False
 
+    def exist_validate(self, validated_data):
+        month = self.validated_data['month']
+        d = datetime.now()
+        if 'year' in validated_data:
+            year = validated_data['year']
+        else:
+            year = d.year
+        room = Room.objects.get(pk=validated_data['room'])
+        query = WaterElectrical.objects.filter(
+            Q(room=room) & 
+            Q(month=month)&
+            Q(year=year)
+        )
+            
+        if len(query) > 0:
+            return False
+        return True     
 
+    def exist_update_validate(self, instance, validated_data):
+        
+        month = self.validated_data['month']
+        d = datetime.now()
+        if 'year' in validated_data:
+            year = validated_data['year']
+        else:
+            year = d.year
+        room = Room.objects.get(pk=validated_data['room'])
+
+        if month == instance.month and year == instance.year and room == instance.room:
+            return True
+        else:
+            query = WaterElectrical.objects.filter(
+                Q(room=room) & 
+                Q(month=month)&
+                Q(year=year)
+            )
+                
+            if len(query) > 0:
+                return False
+            return True     
+
+    def validate(self, data):
+        """
+        validate data
+        """
+        if 'room' not in data:
+            raise serializers.ValidationError({'room' : required_message})
+        if 'water_electrical_unit_price' not in data:
+            raise serializers.ValidationError({'water_electrical_unit_price' : required_message})
+        return data
+
+class WaterElectricalDetailSerializer(serializers.ModelSerializer):
+    water_electrical_unit_price = WaterElectricalUnitPriceSerializer()
+    room = RoomListByAreaSerializer()
+    
+    created_by = UserSerializer(required=False)
+    updated_by = UserSerializer(required=False)
+    class Meta:
+        model = WaterElectrical
+        fields = [
+            'public_id',
+            'room',
+            # 
+            'new_index_eclectrical',
+            'old_index_electrical',
+            # 
+            'new_index_water',
+            'old_index_water',
+            # 
+            'month',
+            'year',
+            # 
+            'water_electrical_unit_price',
+            # 
+            'water_price',
+            'electrical_price',
+            # 
+            'created_at',
+            'last_update',
+            'created_by',
+            'updated_by',
+        ]
