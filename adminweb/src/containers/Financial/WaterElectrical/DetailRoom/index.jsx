98709ac32f2669c6cc64e087a205f7c229b56ba9
Anh To <anh_to@datahouse.com>
2021-05-22 17:04:24 +0700

    Resolve conflict

--- adminweb/src/containers/Financial/WaterElectrical/DetailRoom/index.jsx
+++ adminweb/src/containers/Financial/WaterElectrical/DetailRoom/index.jsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import { makeStyles } from "@material-ui/core/styles";
-import ReactModal from "react-modal";
-import NotificationForm from "./NotificationForm";
 import TextField from "@material-ui/core/TextField";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import PlusIcon from "@material-ui/icons/AddBox";
 import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
 import "./styles.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createWaterElectricalIndex } from "../../../../redux/actions/financial";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 2,
@@ -20,116 +21,43 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
 }));
-function ShowBill({ selectedMonth, selectedYear, area, room }) {
+function ShowBill({ selectedMonth, selectedYear, area, room, onCancel }) {
   const classes = useStyles();
-  const data = {
-    id: 3,
-    name: "101",
-    slug: "101",
-    number_now: 5,
-    typeroom: {
-      id: 1,
-      name: "Type 1",
-      price: 200000,
-      number_max: 8,
-      slug: "type-1",
-    },
-    area: {
-      id: 1,
-      name: "A",
-      slug: "a",
-    },
-    status: "F",
-    created_at: "2021-03-15T14:40:15.339962Z",
-    last_update: "2021-03-18T15:36:20.411397Z",
+  console.log("Room", room.id);
+  const lastWaterElectrical = {
+    room: room.id,
+    month: selectedMonth - 1,
+    year: 2021,
+    new_index_eclectrical: 20,
+    new_index_water: 35,
   };
+
   const [dataSend, setDataSend] = useState({
-    area: area,
-    room: room,
-    newElectricalNumber: 0,
-    oldElectricalNumber: 0,
-    newWaterNumber: 0,
-    oldWaterNumber: 0,
+    room: room.id,
+    new_index_eclectrical: lastWaterElectrical.new_index_eclectrical,
+    new_index_water: lastWaterElectrical.new_index_water,
     month: selectedMonth,
     year: selectedYear,
-    waterUnitPrice: 0,
-    electricalUnitPrice: 0,
-    waterPrice: 0,
-    electricalPrice: 0,
-    total: 0,
-    createdDate: new Date(),
-    description: "",
+    water_electrical_unit_price: 1,
   });
-  const isFull = data.number_now === data.typeroom.number_max;
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const hideModal = () => {
-    setIsModalVisible(false);
+  const handleClickCreate = () => {
+    console.log("DATA SEND", dataSend);
+    createWaterElectricalIndex(dataSend, (output) => {
+      console.log("output", output);
+      if (output.status === "successful") {
+        toast("Tạo thành công");
+        setTimeout(onCancel, 4000);
+      } else toast(output.notification);
+    });
   };
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "50%",
-      bottom: "10%",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
+  const handleClickCancel = () => {
+    onCancel();
   };
-  useEffect(() => {
-    if (
-      dataSend.newElectricalNumber !== 0 &&
-      dataSend.oldElectricalNumber !== 0 &&
-      dataSend.electricalUnitPrice !== 0
-    ) {
-      console.log("vo vong if");
-      setDataSend({
-        ...dataSend,
-        electricalPrice:
-          dataSend.electricalUnitPrice *
-          (dataSend.newElectricalNumber - dataSend.oldElectricalNumber),
-      });
-    }
-  }, [
-    dataSend.newElectricalNumber,
-    dataSend.electricalUnitPrice,
-    dataSend.oldElectricalNumber,
-  ]);
-  useEffect(() => {
-    if (
-      dataSend.newWaterNumber !== 0 &&
-      dataSend.oldWaterNumber !== 0 &&
-      dataSend.waterUnitPrice !== 0
-    ) {
-      console.log("vo vong if");
-      setDataSend({
-        ...dataSend,
-        waterPrice:
-          dataSend.waterUnitPrice *
-          (dataSend.newWaterNumber - dataSend.oldWaterNumber),
-      });
-    }
-  }, [
-    dataSend.newWaterNumber,
-    dataSend.oldWaterNumber,
-    dataSend.waterUnitPrice,
-  ]);
-
-  useEffect(() => {
-    if (dataSend.electricalPrice !== 0 && dataSend.waterPrice !== 0) {
-      setDataSend({
-        ...dataSend,
-        total: dataSend.electricalPrice + dataSend.waterPrice,
-      });
-    }
-  }, [dataSend.electricalPrice, dataSend.waterPrice]);
-  const handleClick = () => {};
-  const handleChange = async (event) => {
+  const handleChange = (event) => {
     const id = event.target.id;
     const data = event.target.value;
-    console.log("name", id, "data", data);
-    setDataSend({ ...dataSend, [id]: data });
+    setDataSend({ ...dataSend, [id]: parseInt(data) });
   };
-  console.log("AAAAAAA", dataSend.month, "BBBBBBBB", dataSend.year);
   return (
     <div className={classes.root}>
       <Box display="flex">
@@ -163,7 +91,7 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
           <TextField
             label="Khu"
             id={"area"}
-            defaultValue={`${dataSend.area}`}
+            defaultValue={`${area}`}
             variant="outlined"
             onChange={handleChange}
             size="small"
@@ -174,7 +102,7 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
           <TextField
             label="Phòng"
             id={"room"}
-            defaultValue={`${dataSend.room}`}
+            defaultValue={`${room.name}`}
             variant="outlined"
             onChange={handleChange}
             size="small"
@@ -194,8 +122,8 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
                 step: 1,
               },
             }}
-            value={`${dataSend.oldElectricalNumber}`}
-            onChange={handleChange}
+            disabled
+            value={`${lastWaterElectrical.new_index_eclectrical}`}
             variant="outlined"
             size="small"
             type="number"
@@ -205,11 +133,11 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
           <TextField
             label="Số nước cũ"
             id={"oldWaterNumber"}
-            value={`${dataSend.oldWaterNumber}`}
+            value={`${lastWaterElectrical.new_index_water}`}
             variant="outlined"
-            onChange={handleChange}
             size="small"
             type="number"
+            disabled
             InputProps={{
               inputProps: {
                 min: 1,
@@ -222,15 +150,19 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
       <Box display="flex">
         <Box marginBottom={3} marginRight={3}>
           <TextField
-            id="newElectricalNumber"
+            id="new_index_eclectrical"
             label="Số điện mới"
             InputProps={{
               inputProps: {
-                min: dataSend.oldElectricalNumber,
+                min: lastWaterElectrical.new_index_eclectrical,
                 step: 1,
               },
             }}
-            value={`${dataSend.newElectricalNumber}`}
+            value={`${
+              dataSend.new_index_eclectrical !== 0
+                ? dataSend.new_index_eclectrical
+                : lastWaterElectrical.new_index_eclectrical
+            }`}
             onChange={handleChange}
             variant="outlined"
             size="small"
@@ -240,123 +172,22 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
         <Box marginBottom={3}>
           <TextField
             label="Số nước mới"
-            id="newWaterNumber"
+            id="new_index_water"
             type="number"
             value={`${
-              dataSend.newWaterNumber !== 0
-                ? dataSend.newWaterNumber
-                : dataSend.oldWaterNumber
+              dataSend.new_index_water !== 0
+                ? dataSend.new_index_water
+                : lastWaterElectrical.new_index_water
             }`}
             variant="outlined"
             onChange={handleChange}
             size="small"
             InputProps={{
               inputProps: {
-                min: dataSend.oldWaterNumber,
+                min: lastWaterElectrical.new_index_water,
                 step: 1,
               },
             }}
-          />
-        </Box>
-      </Box>
-      <Box display="flex">
-        <Box marginBottom={3} marginRight={3}>
-          <TextField
-            id="electricalUnitPrice"
-            label="Giá tiền điện/Số"
-            InputProps={{
-              inputProps: {
-                min: 1000,
-                step: 500,
-              },
-            }}
-            value={`${dataSend.electricalUnitPrice}`}
-            onChange={handleChange}
-            variant="outlined"
-            size="small"
-            type="number"
-          />
-        </Box>
-        <Box marginBottom={3}>
-          <TextField
-            label="Giá tiền nước/Khối"
-            id={"waterUnitPrice"}
-            value={`${dataSend.waterUnitPrice}`}
-            variant="outlined"
-            onChange={handleChange}
-            size="small"
-            type="number"
-            InputProps={{
-              inputProps: {
-                min: 1000,
-                step: 500,
-              },
-            }}
-          />
-        </Box>
-      </Box>
-      <Box display="flex">
-        <Box marginBottom={3} marginRight={3}>
-          <TextField
-            id="electricalPrice"
-            label="Tiền điện"
-            InputProps={{
-              inputProps: {
-                min: 1000,
-                step: 500,
-              },
-            }}
-            value={`${dataSend.electricalPrice}`}
-            onChange={handleChange}
-            variant="outlined"
-            size="small"
-            type="number"
-            disabled
-          />
-        </Box>
-        <Box marginBottom={3}>
-          <TextField
-            label="Tiền nước"
-            id={"waterUnitPrice"}
-            value={`${dataSend.waterPrice}`}
-            variant="outlined"
-            onChange={handleChange}
-            size="small"
-            type="number"
-            disabled
-            InputProps={{
-              inputProps: {
-                min: 1000,
-                step: 500,
-              },
-            }}
-          />
-        </Box>
-      </Box>
-      <Box display="flex" marginTop={5}>
-        <Box marginBottom={3} marginRight={3} style={{ color: "red" }}>
-          <TextField
-            id="total"
-            label="Tổng tiền"
-            value={`${dataSend.electricalPrice}`}
-            onChange={handleChange}
-            style={{ color: "red" }}
-            variant="outlined"
-            size="big"
-            type="number"
-            disabled
-          />
-        </Box>
-        <Box marginBottom={3}>
-          <TextField
-            label="Chi tiết"
-            id={"description"}
-            value={`${dataSend.description}`}
-            variant="outlined"
-            onChange={handleChange}
-            style={{ color: "red" }}
-            size="big"
-            placeholder="Chi tiết"
           />
         </Box>
       </Box>
@@ -364,7 +195,7 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
         <Button
           variant="contained"
           color="secondary"
-          onClick={handleClick}
+          onClick={handleClickCancel}
           style={{ left: "25%" }}
           startIcon={<CancelPresentationIcon></CancelPresentationIcon>}
         >
@@ -373,21 +204,14 @@ function ShowBill({ selectedMonth, selectedYear, area, room }) {
         <Button
           variant="contained"
           color="primary"
-          onClick={handleClick}
+          onClick={handleClickCreate}
           style={{ left: "35%" }}
           startIcon={<PlusIcon></PlusIcon>}
         >
           Tạo hoá đơn
         </Button>
+        <ToastContainer />
       </Box>
-
-      <ReactModal
-        isOpen={isModalVisible}
-        onRequestClose={hideModal}
-        style={customStyles}
-      >
-        <NotificationForm />
-      </ReactModal>
     </div>
   );
 }
