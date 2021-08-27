c3b7b8e96f2a5c937d5ac92a1a0b84bf55e2f18f
Anh To <anh_to@datahouse.com>
2021-08-27 13:28:16 +0700

    edit padding xep lich

--- adminweb/src/containers/ManageUser/Schedule/index.jsx
+++ adminweb/src/containers/ManageUser/Schedule/index.jsx
@@ -228,7 +228,7 @@ const Employee = () => {
                 </div>
                 <div
                   className="col col-two-third"
-                  style={{ width: "fit-content", margin: "3% 0 0 4%" }}
+                  style={{ width: "fit-content", margin: "3% 0 0 10%" }}
                 >
                   <Button
                     variant="contained"
